f78190402278247ed346c3b553fc792c947776a5
@wangxiben <wang.xibeng@peanut.ai>
2018-06-05 13:18:55 +0800

    aaabbb

--- app/src/main/java/com/dgcredit/gitdemo/MainActivity.java
+++ app/src/main/java/com/dgcredit/gitdemo/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("dev1修改222222222++++++++++++");
+        System.out.println("dev1修改211111233dwds22222++++++++++++");
     }
 }
